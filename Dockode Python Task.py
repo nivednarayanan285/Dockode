a0520372dccc1fd09b9535e0445edf07f7834006
Nived Narayanan M <nivednarayanan285@gmail.com>
2024-02-10 12:54:34 +0530

    final task

--- Dockode Python Task.py
+++ Dockode Python Task.py
@@ -1,38 +1,28 @@
 
 def draw_hexagon(rows, columns):
-    
-    if columns % 2 != 0:
-        columns='odd'
-    else:
-        columns='even'
-
     for i in range(rows):
         for j in range(columns // 2):
             print(" ---    ", end="")
-            if columns == "odd" and j == ((columns // 2) - 1):
+            if columns % 2 != 0 and j == ((columns // 2) - 1):
                 print(" ---    ", end="")
         print()
-
         for j in range(columns // 2):
             print("/   \___", end="")
-            if columns == 'odd' and j == ((columns // 2) - 1):
+            if columns % 2 != 0 and j == ((columns // 2) - 1):
                 print("/   \ ", end="")
-            elif columns == 'even' and i != 0 and j == ((columns // 2) - 1):
+            elif columns % 2 == 0 and i != 0 and j == ((columns // 2) - 1):
                 print("/ ", end="")
         print()
-
         for j in range(columns // 2):
             print("\   /   ", end="")
-            if columns == 'odd' and j == ((columns // 2) - 1):
+            if columns % 2 != 0 and j == ((columns // 2) - 1):
                 print("\   /   ", end="")
-            elif columns == 'even' and i != (rows - 1) and j == ((columns // 2) - 1):
+            elif columns % 2 == 0 and i != (rows - 1) and j == ((columns // 2) - 1):
                 print("\ ", end="")
         print()
-
-
     for j in range(columns // 2):
         print(" ---    ", end="")
-        if columns == 'odd' and j == (columns // 2) - 1:
+        if columns % 2 != 0 and j == (columns // 2) - 1:
             print(" ---    ", end="")
 
 
@@ -40,5 +30,5 @@ def draw_hexagon(rows, columns):
 rows = int(input('No of rows'))
 columns = int(input('No of columns'))
 
-# Calling the function to draw the hexagon based on the user input
+# Calling the function to draw the hexagon
 draw_hexagon(rows, columns)
